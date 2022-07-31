4824ac5f64c0d71c1be6457349d7fb5afe229a17
dangra-mohamed <58854717+dangra-mohamed@users.noreply.github.com>
2022-07-31 15:37:14 +0530

    Add files via upload

---
+++ Random No Generator - Palava/Random No Generator/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Random_No_Generator
+{
+    partial class DICL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DICL));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblGenNo = new System.Windows.Forms.Label();
+            this.lstArray = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblGenName = new System.Windows.Forms.Label();
+            this.llblPlLink = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.Firebrick;
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.Color.White;
+            this.btnGenerate.Location = new System.Drawing.Point(221, 296);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(170, 43);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFrom.Location = new System.Drawing.Point(212, 230);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(171, 35);
+            this.txtFrom.TabIndex = 1;
+            this.txtFrom.TextChanged += new System.EventHandler(this.txtFrom_TextChanged);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTo.Location = new System.Drawing.Point(444, 230);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(180, 35);
+            this.txtTo.TabIndex = 2;
+            this.txtTo.TextChanged += new System.EventHandler(this.txtTo_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(395, 231);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2);
+            this.label1.Size = new System.Drawing.Size(49, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "To";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(134, 231);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(2);
+            this.label2.Size = new System.Drawing.Size(78, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "From";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblGenNo
+            // 
+            this.lblGenNo.AutoSize = true;
+            this.lblGenNo.BackColor = System.Drawing.Color.Transparent;
+            this.lblGenNo.Font = new System.Drawing.Font("Palatino Linotype", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenNo.ForeColor = System.Drawing.Color.Black;
+            this.lblGenNo.Location = new System.Drawing.Point(116, 342);
+            this.lblGenNo.Name = "lblGenNo";
+            this.lblGenNo.Padding = new System.Windows.Forms.Padding(10);
+            this.lblGenNo.Size = new System.Drawing.Size(105, 78);
+            this.lblGenNo.TabIndex = 5;
+            this.lblGenNo.Text = "No";
+            this.lblGenNo.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lblGenNo.Click += new System.EventHandler(this.lblGenNo_Click);
+            // 
+            // lstArray
+            // 
+            this.lstArray.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstArray.AutoSize = true;
+            this.lstArray.BackColor = System.Drawing.Color.Firebrick;
+            this.lstArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstArray.ForeColor = System.Drawing.Color.White;
+            this.lstArray.Location = new System.Drawing.Point(216, 501);
+            this.lstArray.Name = "lstArray";
+            this.lstArray.Padding = new System.Windows.Forms.Padding(4);
+            this.lstArray.Size = new System.Drawing.Size(79, 37);
+            this.lstArray.TabIndex = 6;
+            this.lstArray.Text = "array";
+            this.lstArray.Click += new System.EventHandler(this.lstArray_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Firebrick;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(422, 296);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(171, 43);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblGenName
+            // 
+            this.lblGenName.AutoSize = true;
+            this.lblGenName.BackColor = System.Drawing.Color.White;
+            this.lblGenName.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenName.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblGenName.Location = new System.Drawing.Point(213, 420);
+            this.lblGenName.Name = "lblGenName";
+            this.lblGenName.Padding = new System.Windows.Forms.Padding(10);
+            this.lblGenName.Size = new System.Drawing.Size(201, 63);
+            this.lblGenName.TabIndex = 10;
+            this.lblGenName.Text = "Base Price";
+            this.lblGenName.Click += new System.EventHandler(this.lblGenName_Click);
+            // 
+            // llblPlLink
+            // 
+            this.llblPlLink.ActiveLinkColor = System.Drawing.Color.Tomato;
+            this.llblPlLink.AutoSize = true;
+            this.llblPlLink.BackColor = System.Drawing.Color.Transparent;
+            this.llblPlLink.Font = new System.Drawing.Font("Palatino Linotype", 32.25F, System.Drawing.FontStyle.Bold);
+            this.llblPlLink.ForeColor = System.Drawing.Color.Black;
+            this.llblPlLink.LinkColor = System.Drawing.Color.Black;
+            this.llblPlLink.Location = new System.Drawing.Point(211, 351);
+            this.llblPlLink.Name = "llblPlLink";
+            this.llblPlLink.Size = new System.Drawing.Size(277, 58);
+            this.llblPlLink.TabIndex = 5;
+            this.llblPlLink.TabStop = true;
+            this.llblPlLink.Text = "Player Name";
+            this.llblPlLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llblPlLink_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(-312, -114);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(10);
+            this.label5.Size = new System.Drawing.Size(105, 78);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "No";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label5.Click += new System.EventHandler(this.lblGenNo_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.OrangeRed;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(52, 821);
+            this.label6.Name = "label6";
+            this.label6.Padding = new System.Windows.Forms.Padding(4);
+            this.label6.Size = new System.Drawing.Size(79, 37);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "array";
+            this.label6.Click += new System.EventHandler(this.lstArray_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Tomato;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("Palatino Linotype", 32.25F, System.Drawing.FontStyle.Bold);
+            this.linkLabel1.ForeColor = System.Drawing.Color.Black;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(-224, -107);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(277, 58);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Player Name";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llblPlLink_LinkClicked);
+            // 
+            // DICL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1278, 675);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.llblPlLink);
+            this.Controls.Add(this.lblGenName);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lstArray);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblGenNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "DICL";
+            this.Text = "TTC PALAVA";
+            this.Load += new System.EventHandler(this.DICL_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblGenNo;
+        private System.Windows.Forms.Label lstArray;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblGenName;
+        private System.Windows.Forms.LinkLabel llblPlLink;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Random No Generator - Palava/Random No Generator/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_No_Generator
+{
+    public partial class DICL : Form
+    {
+        List<int> genValues =new List<int>();
+        List<Players> PlayersList = new List<Players>();
+        public DICL()
+        {
+            InitializeComponent();
+            
+            lstArray.Text = "";
+            lstArray.MaximumSize = new Size(600, 0);
+            lstArray.AutoSize = true;
+            lblGenName.Text = "";
+            this.WindowState = FormWindowState.Maximized;
+
+            //int height = Screen.PrimaryScreen.Bounds.Height;
+            //int width = Screen.PrimaryScreen.Bounds.Width;
+
+            //WindowState = FormWindowState.Maximized;
+            //MinimumSize = this.Size;
+            //MaximumSize = this.Size;
+
+            GetAllUsers();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (txtFrom.Text == "" || txtTo.Text == "")
+                MessageBox.Show("Please provide From and To value.");
+            else if((Convert.ToInt32(txtTo.Text)) < (Convert.ToInt32(txtFrom.Text)))
+                MessageBox.Show("From Value is greater than To value.");
+            else if (Convert.ToInt32(txtTo.Text) > 56)
+                MessageBox.Show("To Value should not be greater than 56.");
+            else
+            {
+                if ((Convert.ToInt32(txtTo.Text)+ 1) - (Convert.ToInt32(txtFrom.Text)) != genValues.Count)
+                {
+                    Random rnd = new Random();
+                    int value = 0;
+                    do
+                    {
+                        value = rnd.Next(Convert.ToInt32(txtFrom.Text), Convert.ToInt32(txtTo.Text) + 1);
+
+                        if (genValues.Count == 0)
+                            break;
+
+                    } while (genValues.Contains(value));
+
+                    genValues.Add(value);
+
+                    Players Pl = new Players();
+                    Pl = PlayersList.Where(m => m.Id == value).FirstOrDefault();
+
+                    lblGenNo.Text = value.ToString() + ".";
+                    lblGenName.Text = "";
+                    llblPlLink.ImageKey = Pl.BasePrice;
+                    llblPlLink.Text = Pl.Name;
+                    lblGenName.Text = Pl.BasePrice;
+                    lstArray.Text = "";
+                    foreach (int a in genValues)
+                    {
+                        lstArray.Text += a.ToString() + " | ";
+                    }
+                    lstArray.ToString().TrimEnd('|');
+                    //if(Pl.Retained != "0")
+                    //{
+                    //    lblGenName.Text = Pl.Name + " is Retained";
+                    //}
+                }
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+
+        private void clearAll()
+        {
+            genValues.Clear();
+            lstArray.Text = "";
+            lblGenNo.Text = "No";
+            lblGenName.Text = "";
+            txtFrom.Text = "";
+            txtTo.Text = "";
+            llblPlLink.Text = "Player Name";
+        }
+        private void txtFrom_TextChanged(object sender, EventArgs e)
+        {
+            genValues.Clear();
+            lstArray.Text = "";
+            lblGenNo.Text = "No";
+            lblGenName.Text = "";
+            llblPlLink.Text = "Player Name";
+        }
+
+        private void txtTo_TextChanged(object sender, EventArgs e)
+        {
+            genValues.Clear();
+            lstArray.Text = "";
+            lblGenNo.Text = "No";
+            lblGenName.Text = "";
+            llblPlLink.Text = "Player Name";
+        }
+
+        private void GetAllUsers()
+        {            
+            PlayersList.Add(new Players(1, "Nabakishore Prusty", "Base Amount 500K", "0"));
+            PlayersList.Add(new Players(2, "Dheeraj More", "Base Amount 500K", "0"));
+            PlayersList.Add(new Players(3, "Akash Giri", "Base Amount 500K", "0"));
+            PlayersList.Add(new Players(4, "Pratheek Acharya", "Base Amount 500K", "0"));
+            PlayersList.Add(new Players(5, "Shubhank Manoj", "Base Amount 500K", "0"));
+            PlayersList.Add(new Players(6, "Navneet Padhy", "Base Amount 500K", "0"));
+            PlayersList.Add(new Players(7, "Agrim Vaibhav", "Base Amount 500K", "0"));
+            PlayersList.Add(new Players(8, "Shailesh Jha", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(9, "Ramesh  Subramanian", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(10, "Pushkaraj Badbade", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(11, "Ravikumar Ganti", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(12, "Sadananda Nayak", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(13, "Saumil Bhavsar", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(14, "Chhannoo lal Verma", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(15, "Akhilesh Pendharkar", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(16, "Santosh Shelke", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(17, "Ramesh Mandal", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(18, "Rajiv Pillai", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(19, "Tapan Singh", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(20, "Divyam  Dama", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(21, "Anmol Rai", "Base Amount 200K", "0"));
+            PlayersList.Add(new Players(22, "Dr Sudhakar Babras", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(23, "Devaki nandan Aggarwal", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(24, "Vinod Dighe", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(25, "Vijay Kumar", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(26, "Kalyanam Narayanswamy", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(27, "Shivram Iyer", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(28, "Valayapathy Thangaraju", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(29, "Prakash Shinde", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(30, "Sham d lone Lone", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(31, "Rajesh Singh", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(32, "Pradeep Dubey", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(33, "Abhrajit Kundu", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(34, "Nagraj Wadimath", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(35, "DUMMY", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(36, "Ashish Pandey", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(37, "Tripurari Pratap", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(38, "Abhishek Bajpayee", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(39, "Swapnil Gawde", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(40, "Atul Khairnar", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(41, "Pratik Shevle", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(42, "Ankit Agarwal", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(43, "Pearl  Dhamesha", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(44, "Swati  Sharma", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(45, "Lekha mishra  Hedge", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(46, "DUMMY", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(47, "Sadiya Vanjara", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(48, "Arya Talaulikar", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(49, "Dhruv Narula", "Base Amount 100K", "0"));
+            PlayersList.Add(new Players(50, "Pratik Salian", "Base Amount 50K", "0"));
+            PlayersList.Add(new Players(51, "Anish Shirke", "Base Amount 50K", "0"));
+            PlayersList.Add(new Players(52, "DUMMY", "Base Amount 50K", "0"));
+            PlayersList.Add(new Players(53, "Harshal Salian", "Base Amount 50K", "0"));
+            PlayersList.Add(new Players(54, "Dhruv Maheshwari", "Base Amount 50K", "0"));
+            PlayersList.Add(new Players(55, "Vedant Misal", "Base Amount 50K", "0"));
+            PlayersList.Add(new Players(56, "Saransh Dwivedi", "Base Amount 50K", "0"));
+
+
+
+
+        }
+
+        private void llblPlLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //System.Diagnostics.Process.Start(((System.Windows.Forms.LinkLabel)sender).ImageKey);
+        }
+
+        private void lblGenNo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DICL_Load(object sender, EventArgs e)
+        {
+            lblGenName.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGenName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstArray_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Players
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BasePrice { get; set; }
+
+        public string Retained { get; set; }
+
+        public Players()
+        {
+
+        }
+        public Players(int ID, string Name, string Link, string retained)
+        {
+            this.Id = ID;
+            this.Name = Name;
+            this.BasePrice = Link;
+            this.Retained = retained;
+
+        }
+    }
+}
